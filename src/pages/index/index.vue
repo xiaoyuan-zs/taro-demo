ef9c7fa03054b3d31bb08ff874798dbeec746da8
xiaoyuan-zs <yuandh@hzcacs.com>
2025-04-29 17:20:36 +0800

    feat: init commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <view>
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import './index.scss'
+
+const msg = ref('Hello world')
+</script>
